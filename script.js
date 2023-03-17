193102c2a1bc1d1d39ba457fb397690a73cb1d5e
selim <nfc.selim@gmail.com>
2023-03-17 23:55:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let content = document.getElementById('addTask');
+
+document.getElementById('saveTask').onclick = function() {
+
+    if(document.getElementById('addTask').value == ""){
+        document.getElementById('add-new-todo').style.border  = 'solid red';
+    }else{
+        document.getElementById('add-new-todo').style.border  = 'none';
+        let addTask = document.getElementById('add-task').innerHTML += 
+        `<div onclick="taskDone()" id="added-todo" class="added-new-todo"><p class="saved-task-text" type="text">${content.value}</p><button id="taskDone" class="added-task-done">✓</button></div>`;
+    }
+
+}
+
+//Remove Task
+function taskDone(){
+    let task = document.getElementById('added-todo');
+    task.remove();
+}
