585a5b9d2198fd0f8e666d13abc0d75c5265d666
selim <nfc.selim@gmail.com>
2023-03-18 15:15:02 +0100

    Input Update

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ document.getElementById('saveTask').onclick = function() {
         document.getElementById('add-new-todo').style.border  = 'solid red';
     }else{
         document.getElementById('add-new-todo').style.border  = 'none';
+        
         let addTask = document.getElementById('add-task').innerHTML += 
         `<div onclick="taskDone()" id="added-todo" class="added-new-todo"><p class="saved-task-text" type="text">${content.value}</p><button id="taskDone" class="added-task-done">✓</button></div>`;
+    
+        document.getElementById('addTask').value = '';
     }
 
 }
