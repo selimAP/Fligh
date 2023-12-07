b447b90ce8881d38a63885d6caeacf156a6bc2cc
selim <nfc.selim@gmail.com>
2023-12-07 23:08:34 +0100

    Code structured and lastDeleted inserted

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let content = document.getElementById('addTask');
+const content = document.getElementById('addTask');
+const saveTaskBtn = document.getElementById('saveTask');
+const lastDeletedTodoContainer = document.getElementById('add-task');
+const deletedTaskContainer = document.getElementById('deleted-task');
 let taskId = 0;
 
 content.addEventListener("keypress", function(event) {
@@ -8,30 +11,57 @@ content.addEventListener("keypress", function(event) {
       }
     });
 
-document.getElementById('saveTask').onclick = function() {
-    
-    if(document.getElementById('addTask').value == ""){
-        document.getElementById('add-new-todo').style.border  = 'solid red';
-    }else{
-        document.getElementById('add-new-todo').style.border  = 'none';
+    saveTaskBtn.addEventListener('click', function () {
 
+        const addTaskInput = document.getElementById('addTask');
+        const addNewTodo = document.getElementById('add-new-todo');
         const datetimeValue = document.getElementById('datetime').value;
-
-        let addTask = document.getElementById('add-task').innerHTML += 
-        `<div id="added-todo-${taskId}" class="added-new-todo"><p id="saved-task-text" class="saved-task-text" type="text">${content.value}</p><span id="taskDate-${taskId}" class="date-span">${datetimeValue}</span><button onclick="taskDone(event)" id="taskDone-${taskId}" class="added-task-done">🗑️</button></div>`;
     
+    if (addTaskInput.value === '') {
+        addNewTodo.style.border = 'solid red';
+    }else{
+        const currentTaskId = taskId;
+
+        addNewTodo.style.border = 'none';
+
+        const newTodoHTML  = document.getElementById('add-task').innerHTML += `
+        <div id="added-todo-${currentTaskId}" class="added-new-todo">
+            <p class="saved-task-text" type="text">${addTaskInput.value}</p>
+            <span id="taskDate-${currentTaskId}" class="date-span">${datetimeValue}</span>
+            <button id="taskDone-${currentTaskId}" class="added-task-done task-done-btn">🗑️</button>
+        </div>`;
+
         taskId++;
-        document.getElementById('addTask').value = '';
+        addTaskInput.value = '';
 
-        let taskDate = document.getElementById(`taskDate-${taskId-1}`);
-        if(taskDate && taskDate.innerHTML === '00:00'){
-            taskDate.style.display = "none"; 
-            document.getElementById('saved-task-text').style.minWidth = "85%";
+        const taskDate = document.getElementById(`taskDate-${currentTaskId}`);
+        const savedTaskText = document.querySelector('.saved-task-text');
+
+        if (taskDate && taskDate.innerHTML === '00:00') {
+            taskDate.style.display = 'none';
+            savedTaskText.style.minWidth = '85%';
         }
-    }
-}
+  }
+});
 
-function taskDone(event){
-    let task = event.target.parentElement;
-    task.remove();
+lastDeletedTodoContainer.addEventListener('click', function (event) {
+    const taskDoneBtn = event.target;
+    if (taskDoneBtn.classList.contains('added-task-done')) {
+      const task = taskDoneBtn.parentElement;
+      const todoId = task.id.split('-')[2]; // Extract the taskId
+  
+      taskDoneBtn.remove();
+      task.remove();
+  
+      deletedTaskContainer.innerHTML += `
+        <div id="deleted-todo-${todoId}" class="added-new-todo">
+          ${task.innerHTML}
+          <button onclick="deleteTaskPermnt(event)" class="added-task-done task-done-btn">🗑️</button>
+        </div>`;
+    }
+  });
+
+function deleteTaskPermnt(event) {
+  const deletePermBtn = event.target.parentElement;
+  deletePermBtn.remove();
 }
